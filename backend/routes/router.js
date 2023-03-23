e4d4a8f847dffc2981c8ae978ce71b44531afd9a
LuanSilva669 <125572605+LuanSilva669@users.noreply.github.com>
2023-03-23 10:40:15 -0300

    Finalizado

---
+++ backend/routes/router.js
@@ -0,0 +1,13 @@
+const router = require("express").Router()
+ 
+// Services router
+const servicesRouter = require("./services")
+
+router.use("/", servicesRouter)
+
+// Parties routes
+const partyRouter = require("./parties");
+
+router.use("/", partyRouter);
+
+module.exports = router;
